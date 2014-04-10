25ce804940a0632c65bd5ee49ae84b228ed021ce
igor.kvazar <igor.kvazar@gmail.com>
2014-04-10 10:43:19 +0300

    added history direction - FORWARD & BACK

---
+++ genealogia/enums/Direction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package genealogia.enums;
+
+/**
+ *
+ * @author kvazar
+ */
+public enum Direction {
+    Forward,
+    Back    
+}
